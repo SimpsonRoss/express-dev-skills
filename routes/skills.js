4c1e833fa7ffeee19afcfa471eabba444a2100bf
SimpsonRoss <thisisrosssimpson@gmail.com>
2023-07-05 11:13:14 -0400

    Adding the functionality to delete, edit, update skills. Plus updating some more bootstrap styling

--- routes/skills.js
+++ routes/skills.js
@@ -4,7 +4,17 @@ var skillsCtrl = require('../controllers/skills');
 
 // GET /skills
 router.get('/', skillsCtrl.index);
+// GET /skills/new
+router.get('/new', skillsCtrl.new);
 // GET /skills/:id
 router.get('/:id', skillsCtrl.show);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// GET /skills/:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+// PUT /skillss/:id
+router.put('/:id', skillsCtrl.update);
 
 module.exports = router;
